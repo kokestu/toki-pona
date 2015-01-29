760f7fbea73eac6db658f52a22cb1a5c249d5a2f
js7814 <js7814@ic.ac.uk>
2015-01-29 15:56:01 +0000

    Implemented full enumerated data types.

--- TokiPona.hs
+++ TokiPona.hs
@@ -2,6 +2,31 @@ module TokiPona where
 
 import Data.Maybe
 
-data Word = 
+data Sentence = NullSentence | Interj Word | SentInd SubCl Voc Subj Pred |
+                SentVoc SubCl Voc Pred
 
-data Structure = 
+data SubCl    = NullSubCl | SentLa Sentence | NounLa NounPhr
+
+data Voc      = NullVoc | O NounPhr
+
+data Subj     = Mi | Sina | Li NounPhr
+
+data Pred     = Being NounPhr PrepPhr | Doing VerbPhr PrepPhr |
+                And Pred Pred | Or Pred Pred
+
+data NounPhr  = NullNounPhr | Mod Word Modif | Pi NounPhr NounPhr | 
+                NConj NounPhr Conj NounPhr
+
+data Conj     = Anu | En
+
+data PrepPhr  = NullPrepPhr | Dir Prep NounPhr
+
+data VerbPhr  = Verb Verbal | VerbalMod Modal Verbal | NegVerbal Verbal |
+                NegModal Modal Verbal
+
+data Verbal   = VerbMod Word Modif | VerbTMod Word Modif Obj | Lon NounPhr |
+                Tawa NounPhr
+
+data Modal    = Kama | Ken | Wile
+
+data Obj      = E NounPhr
